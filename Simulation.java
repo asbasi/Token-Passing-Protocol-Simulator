9bd184b9c6f94ffff2ed6ed8aa8042040aafb449
Arvinder Basi <arvinder.singh.basi@gmail.com>
2017-01-05 15:22:50 -0800

    Update Simulation.java

--- Simulation.java
+++ Simulation.java
@@ -22,18 +22,18 @@ public class Simulation
         int N = 0;  // Number of hosts.
         
         double currentTime = 0.0;   // The current time.
-	    int currentHost = 0;	    // The current host.
-	    int currentType = 0;	    // The current event type.
+        int currentHost = 0;	    // The current host.
+        int currentType = 0;	    // The current event type.
 
-	    double totalTime = 0.0;	// Total time of the simulation 
+        double totalTime = 0.0;	// Total time of the simulation 
 
-	    int queueSize = 0;    // Size of a particular host's queue.
+        int queueSize = 0;    // Size of a particular host's queue.
         
         double aRate = 0.0;   // Arrival rate lambda (packets/second).
         
         int numPackets = 0;   // Number of packets that were delivered.
 
-	    double transmission_delay = 0.0;    // Transmission delay of frame.
+        double transmission_delay = 0.0;    // Transmission delay of frame.
 
         double averagePacketDelayAcc = 0.0;	// Accumulator variable for the packet delay.        
         long totalBytesTransferred = 0; 	// Accumulator variable for the throughput. 
@@ -87,44 +87,44 @@ public class Simulation
             }
             else // Token Arrival
             {
-		        queueSize = queue.get(currentHost).size();
+                queueSize = queue.get(currentHost).size();
 		        
-		        // Have something to send.
+	        // Have something to send.
                 if(queueSize > 0) 
                 {
-		            // Calculate the total transmission time of the frame.
-		            for(int i = 0; i < queueSize; i++)
-		            {
-		                transmission_delay += (queue.get(currentHost).get(i).getSize() * 8) / TRANSMISSION_RATE;
-		            }
+	            // Calculate the total transmission time of the frame.
+	            for(int i = 0; i < queueSize; i++)
+	            {
+                        transmission_delay += (queue.get(currentHost).get(i).getSize() * 8) / TRANSMISSION_RATE;
+	            }
 	            
                     // Calculate the throughput/average packet delay for each of the packets being sent. 
-		            for(int i = 0; i < queueSize; i++)
-		            {
-			            numPackets++;
-		    	        currentPacket = queue.get(currentHost).remove();
-			            totalBytesTransferred += currentPacket.getSize();
-			            averagePacketDelayAcc += ((currentPacket.getDestination() + N - currentHost) % N) 
-			                                      * (transmission_delay + PROP_DELAY) - 
-			                                      currentPacket.getArrivalTime() + currentTime;
+		    for(int i = 0; i < queueSize; i++)
+	            {
+                        numPackets++;
+                        currentPacket = queue.get(currentHost).remove();
+                        totalBytesTransferred += currentPacket.getSize();
+                        averagePacketDelayAcc += ((currentPacket.getDestination() + N - currentHost) % N) 
+                                      * (transmission_delay + PROP_DELAY) - 
+                                      currentPacket.getArrivalTime() + currentTime;
 			            
-			            // Simulated the desired number of packets so we stop.
-			            if(numPackets == NUM_DESIRED)
-			            {
-				            totalTime = ((currentPacket.getDestination() + N - currentHost) % N) 
-				                        * (transmission_delay + PROP_DELAY) + currentTime;
-				            break;			
-			            }
+                        // Simulated the desired number of packets so we stop.
+                        if(numPackets == NUM_DESIRED)
+                        {
+                            totalTime = ((currentPacket.getDestination() + N - currentHost) % N) 
+                                        * (transmission_delay + PROP_DELAY) + currentTime;
+                            break;			
+                        }
                     }
 	
-		            // Create next token arrival event.
+                    // Create next token arrival event.
                     gel.insert(new Event(TKN_ARRIVAL, (currentHost + 1)  % N, currentTime 
                                 + (N *(transmission_delay + PROP_DELAY)) + PROP_DELAY));
 
-		            transmission_delay = 0;
+                    transmission_delay = 0;
                 }
                 else // Nothing to send so we just pass the token.
-                {
+		{
                     gel.insert(new Event(TKN_ARRIVAL, (currentHost + 1)  % N, currentTime + PROP_DELAY));
                 }
             }
@@ -132,7 +132,7 @@ public class Simulation
 	
         // Print out all the performance measures. 
         System.out.println("\nNumber of Packets Processed: " + numPackets);
-	    System.out.println("Throughput: " + (totalBytesTransferred / totalTime));
+	System.out.println("Throughput: " + (totalBytesTransferred / totalTime));
         System.out.println("Average Packet Delay: " + (averagePacketDelayAcc / numPackets));
     }
     
